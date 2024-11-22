1ea016f681d37fa6c9c6e311cbc224555086e533
rsproj7 <rodolfoparanhos92@gmail.com>
2024-11-21 21:21:20 -0300

    up

---
+++ start.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const numeral = require('numeral');
+const shelljs = require('shelljs');
+
+const exec = shelljs.exec;
+
+const roodir = __dirname,
+      scriptdir = path.join(roodir, 'script');
+      
+console.log("Executing: rsp-fsystem/start.js")
+
+fs.readdirSync(scriptdir).filter(file => {
+    return numeral(file[0]).value() || numeral(file[0]).value() === 0;
+}).forEach(file => {
+    exec(`sh ${file}`)
+})
+
+console.log("Finished execution of: rsp-fsystem/start.js")
